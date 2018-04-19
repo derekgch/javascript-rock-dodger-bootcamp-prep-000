aebef88d937cdd828aa59e30401a291bcec8613a
derek chen <derek.g.chen@gmail.com>
2018-04-19 19:04:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ function endGame() {
 
   clearInterval(gameInterval);
   while (ROCKS.length > 0) {
-    ROCKS.removes();
+    ROCKS.pop();
   }
   window.removeEventListener('keydown', moveDodger);
   console.log("YOU LOSE!");

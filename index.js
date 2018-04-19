57bcac3c8e871d61e8d28665aaacff6be8bc4732
derek chen <derek.g.chen@gmail.com>
2018-04-19 19:18:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ function moveDodgerRight() {
     }else {
       right_limit = 180
     }
-    if(right_limit < 181 ) {
+    if(right_limit < 180 ) {
 
      function step() {
        DODGER.style.right = `${right += 1}px`

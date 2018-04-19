f5c68fa02f445baa5791ee2222fc2065b378e1cb
derek chen <derek.g.chen@gmail.com>
2018-04-19 16:57:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ function createRock(x) {
      */
        rock.style.top = `${top += 2}px`
 
-       if (top < 200) {
+       if (top < 360) {
          window.requestAnimationFrame(moveRock)
        }
      }

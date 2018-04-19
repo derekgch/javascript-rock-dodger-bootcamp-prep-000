50afd0c53fa0e7247e1fb66812b9708e4adb9ae7
derek chen <derek.g.chen@gmail.com>
2018-04-19 20:06:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-     if(checkCollision(rock)){
+     if(checkCollision(rock) == true){
        endGame();
        return;
      }

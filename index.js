d2c6b6c37f3b03f383acf09e0ce11e79788b59f1
derek chen <derek.g.chen@gmail.com>
2018-04-19 17:33:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-  
+
 
     window.addEventListener('keydown', function(e) {
       if (e.which === LEFT_ARROW) {
